a4bd4cf6c40323ea0d755752d68b90b114872bdd
chaixshot <chai5142xshot@gmail.com>
2026-06-29 16:25:41 +0700

    Changed: TwoHandedMode.MoveToTakeControl

--- Patches/Pointer/TwoHandedMode.cs
+++ Patches/Pointer/TwoHandedMode.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Runtime.CompilerServices;
+using UnityEngine;
 using uWindowCapture;
 using XSOverlay;
 using xsoverlay_tweak.Utils;
@@ -8,6 +10,11 @@ namespace xsoverlay_tweak.Patches.Pointer
     [HarmonyPatch(typeof(Raycaster))]
     internal class TwoHandedMode
     {
+        private class RaycasterData
+        {
+            public Vector3 LastRayHitPoint;
+        }
+        private static readonly ConditionalWeakTable<Raycaster, RaycasterData> RaycasterDictionary = new();
 
         [HarmonyPatch(typeof(Raycaster), "HandleClicksForDesktopWindows"), HarmonyPatch(typeof(Raycaster), "HandleTouchInputForDesktopWindows")]
         [HarmonyPrefix]
@@ -23,6 +30,24 @@ namespace xsoverlay_tweak.Patches.Pointer
                 MouseOperations.SetCursorPosition((int)desktopCoordinate.Value.desktopCoord.x, (int)desktopCoordinate.Value.desktopCoord.y);
 
                 __instance.CanClickDesktopCursor = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Raycaster), "OnDesktopCursor")]
+        [HarmonyPrefix]
+        public static void MoveToTakeControl(Raycaster __instance, Vector3 ___RayHitPoint)
+        {
+            if (!IsEnable()) return;
+
+            if (DesktopCursorManager.Instance.GetCurrentInputDevice() != __instance)
+            {
+                RaycasterData data = RaycasterDictionary.GetOrCreateValue(__instance);
+
+                if (Vector3.Distance(___RayHitPoint, data.LastRayHitPoint) > 0.01f)
+                {
+                    data.LastRayHitPoint = ___RayHitPoint;
+                    XSOEventSystem.Current.EventTakeControlOfDesktopCursor(__instance);
+                }
             }
         }
 
