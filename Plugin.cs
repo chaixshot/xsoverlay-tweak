8157c0bbe560151a9779560254241691712ddb09
chaixshot <chai5142xshot@gmail.com>
2026-07-07 19:33:26 +0700

    Removed: Patches.Fix.AdditionalFix

--- Plugin.cs
+++ Plugin.cs
@@ -89,7 +89,6 @@ public class Plugin : BaseUnityPlugin
         harmony.PatchAll(typeof(Patches.QualityOfLife.WindowsAccentColor));
 
         // Fix
-        harmony.PatchAll(typeof(Patches.Fix.AdditionalFix));
         harmony.PatchAll(typeof(Patches.Fix.CtrlKeyStickyFix));
         harmony.PatchAll(typeof(Patches.Fix.CursorMovingInteractionFix));
         harmony.PatchAll(typeof(Patches.Fix.HandleScrollingFix));
