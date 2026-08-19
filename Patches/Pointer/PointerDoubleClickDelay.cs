b48c751c3c19b09a40162b203b522b84af05e71f
chaixshot <chai5142xshot@gmail.com>
2026-08-19 17:37:24 +0700

    Update: PointerDoubleClickDelay

--- Patches/Pointer/PointerDoubleClickDelay.cs
+++ Patches/Pointer/PointerDoubleClickDelay.cs
@@ -1,16 +1,18 @@
 ﻿using HarmonyLib;
 using XSOverlay;
+using xsoverlay_tweak.Utils;
 
 namespace xsoverlay_tweak.Patches.Pointer
 {
     [HarmonyPatch(typeof(Raycaster))]
     internal class PointerDoubleClickDelay
     {
-        [HarmonyPatch("SetVisualCursorTransform")]
+        [HarmonyPatch("SearchForOverlays")]
         [HarmonyPrefix]
-        public static bool BlockPointerMovement(ref MouseInputDevice ___InputDevice)
+        public static bool BlockPointerMovement(Raycaster __instance, ref MouseInputDevice ___InputDevice)
         {
             if (!IsEnable()) return true;
+            if (EventBridge.IsOverlayWebView(__instance.HoveringOverlay)) return true;
 
             return !___InputDevice.ClickFreezeActive;
         }
