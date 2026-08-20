cec555f6d6ef77b368e253e60bed50245ce4d5e1
chaixshot <chai5142xshot@gmail.com>
2026-08-20 17:36:48 +0700

    Update: CtrlKeySticky

--- Patches/Keyboard/CtrlKeySticky.cs
+++ Patches/Keyboard/CtrlKeySticky.cs
@@ -1,22 +1,45 @@
 ﻿using HarmonyLib;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using XSOverlay;
+using XSOverlay.WebApp;
+using XSOverlay.Websockets.API;
 
 namespace xsoverlay_tweak.Patches.Keyboard
 {
+    [HarmonyPatch(typeof(ApiHandler))]
     internal class CtrlKeySticky
     {
-        [HarmonyPatch(typeof(KeyboardKey), "Start")]
-        [HarmonyPostfix]
-        public static void AddCtrlKeySticky(KeyboardKey __instance)
+        [HarmonyPatch(typeof(UpdateDateTime), "Awake")]
+        [HarmonyPrefix]
+        public static void ListenCofigChange()
         {
-            if (__instance.Label == "ctrl")
+            XConfig.CtrlKeySticky.SettingChanged += (sender, args) =>
             {
-                __instance.IsDoubleTappable = IsEnable();
+                ServerClientBridge.Instance.Api.Commands["OnRequestKeyboardLayout"]("", "", "");
+            };
+        }
 
-                XConfig.CtrlKeySticky.SettingChanged += (sender, args) =>
+        [HarmonyPatch(typeof(ApiHandler), "OnRequestKeyboardLayout")]
+        [HarmonyPrefix]
+        public static bool HandleCtrlKeySticky(ApiHandler __instance, string sender)
+        {
+            KeyboardApiObject keyboardInfo = KeyboardHelper.GetKeyboardLayoutInfo();
+
+            foreach (List<KeyboardKey> keyboardKeys in keyboardInfo.mainKeys)
+            {
+                foreach (KeyboardKey key in keyboardKeys)
                 {
-                    __instance.IsDoubleTappable = IsEnable();
-                };
+                    if (key.keycode == "LCONTROL" || key.keycode == "RCONTROL")
+                        key.isDoubleTappable = IsEnable();
+                }
             }
+
+            string data2 = JsonConvert.SerializeObject(keyboardInfo);
+
+            __instance.SendMessage("UpdateKeyboardLayout", data2, null, sender);
+
+            return false;
         }
 
         private static bool IsEnable()
