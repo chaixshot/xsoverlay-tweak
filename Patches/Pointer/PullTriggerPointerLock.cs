fd6a41d92d242d6fe3b122f33fc8008a95d09bd4
chaixshot <chai5142xshot@gmail.com>
2026-08-21 20:18:24 +0700

    Changed: inproved PullTriggerPointerLock

--- Patches/Pointer/PullTriggerPointerLock.cs
+++ Patches/Pointer/PullTriggerPointerLock.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using System;
 using System.Collections;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using XSOverlay;
@@ -16,19 +17,20 @@ namespace xsoverlay_tweak.Patches.Pointer
             public bool IsStopping = false;
             public bool IsDown = false;
             public bool WasSmooth = false;
+            public bool WasClick = false;
             public Vector2 DesktopCoordinates = new();
             public Coroutine Coroutine;
         }
         public static readonly ConditionalWeakTable<Raycaster, RaycasterState> InstanceState = new();
 
         private static readonly Func<Raycaster, float> GetTriggerAxis = AccessTools.MethodDelegate<Func<Raycaster, float>>(AccessTools.Method(typeof(Raycaster), "GetTriggerAxis"));
-        private static float pointerSmoothing = XSettingsManager.Instance.Settings.PointerSmoothing;
+        private static float defaultSmoothing = XSettingsManager.Instance.Settings.PointerSmoothing;
 
         [HarmonyPatch(typeof(Raycaster), "Start")]
         [HarmonyPostfix]
-        public static void Init(Raycaster __instance)
+        public static void Initiation(Raycaster __instance)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return;
+            if (!EventBridge.IsRaycasterHand(__instance)) return;
 
             InstanceState.Add(__instance, new());
 
@@ -36,8 +38,11 @@ namespace xsoverlay_tweak.Patches.Pointer
             {
                 if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
                 {
+                    if (Data.Coroutine != null)
+                        __instance.StopCoroutine(Data.Coroutine);
+
                     Data.IsStopping = true;
-                    Data.Coroutine = Plugin.Instance.StartCoroutine(UnblockDelay(__instance));
+                    Data.Coroutine = __instance.StartCoroutine(UnblockDelay(__instance));
                 }
             };
         }
@@ -46,12 +51,22 @@ namespace xsoverlay_tweak.Patches.Pointer
         [HarmonyPostfix]
         public static void ListenTriggerAxis(Raycaster __instance, bool ___HadMouseInputDown, bool ___HoldingTouch, bool ___IsWebViewTouchEventDown)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return;
+            if (!IsEnable()) return;
             if (!EventBridge.IsActiveHand(__instance)) return;
             if (!InstanceState.TryGetValue(__instance, out RaycasterState Data)) return;
 
             Unity_Overlay hovering = __instance.HoveringOverlay;
-            if (hovering == null || hovering.IsHeld || hovering.IsLocked || __instance.HeldOverlay != null) return;
+            if (hovering == null || hovering.IsHeld || hovering.IsLocked || __instance.HeldOverlay != null)
+            {
+                // Force reset state if hovering state was invalidated mid-pull
+                if (Data.IsBlock || Data.WasSmooth)
+                {
+                    if (Data.Coroutine != null)
+                        __instance.StopCoroutine(Data.Coroutine);
+                    Data.Coroutine = __instance.StartCoroutine(UnblockDelay(__instance));
+                }
+                return;
+            }
 
             bool isDesktopOrCapture = hovering.IsDesktopOrWindowCapture;
             bool isWebViewLock = XConfig.PullTriggerPointerLock.Value == 2 && hovering.IsPluginApplication;
@@ -61,39 +76,36 @@ namespace xsoverlay_tweak.Patches.Pointer
             float axisValue = GetTriggerAxis(__instance);
             Data.IsDown = ___HadMouseInputDown || ___HoldingTouch || ___IsWebViewTouchEventDown;
 
-            if (XConfig.PullTriggerPointerLock.Value >= 3) // Smooth Mode
+            if (IsSmoothMode())
             {
                 Data.IsDown = Data.IsDown || axisValue >= XConfig.PullTriggerClickThreshold.Value;
 
-                if (axisValue > 0f)
+                if (axisValue > 0f) // Start Pull
                 {
                     if (isWebViewSmooth)
                         hovering.UseCursorSmoothing = isWebViewSmooth;
 
-                    if (Data.IsStopping)
-                        Plugin.Instance.StopCoroutine(Data.Coroutine);
+                    if (Data.IsStopping && Data.Coroutine != null)
+                        __instance.StopCoroutine(Data.Coroutine);
 
-                    if (!Data.WasSmooth)
-                    {
-                        pointerSmoothing = XSettingsManager.Instance.Settings.PointerSmoothing;
-                        XSettingsManager.Instance.Settings.PointerSmoothing = 10f;
-                    }
+                    if (defaultSmoothing <= 0.85f) // Do smooth when pulling, less smooth when click holding
+                        XSettingsManager.Instance.Settings.PointerSmoothing = Data.IsDown ? 0.85f : 1.0f;
 
-                    Data.IsStopping = false;
                     Data.WasSmooth = true;
+                    Data.IsStopping = false;
                 }
-                else if (Data.WasSmooth && !Data.IsStopping)
+                else if (Data.WasSmooth && !Data.IsStopping) // Releasing Pull
                 {
                     Data.IsStopping = true;
-                    Data.Coroutine = Plugin.Instance.StartCoroutine(UnblockDelay(__instance));
+                    Data.Coroutine = __instance.StartCoroutine(UnblockDelay(__instance));
                 }
             }
-            else
+            else if (IsLockMode())
             {
-                if (axisValue > 0f && !Data.IsDown) // Lock Mode
+                if (axisValue > 0f && !Data.IsDown) // Start Pull
                 {
-                    if (Data.IsStopping)
-                        Plugin.Instance.StopCoroutine(Data.Coroutine);
+                    if (Data.IsStopping && Data.Coroutine != null)
+                        __instance.StopCoroutine(Data.Coroutine);
 
                     if (!Data.IsBlock)
                         AdvancedHaptics.Rumble(__instance.HapticDeviceName == Raycaster.HapticDevice.Left, 0.001f, 320f, XConfig.PullTriggerPointerLockHaptic.Value / 100f);
@@ -101,14 +113,14 @@ namespace xsoverlay_tweak.Patches.Pointer
                     Data.IsStopping = false;
                     Data.IsBlock = true;
                 }
-                else if (Data.IsBlock && !Data.IsStopping)
+                else if (Data.IsBlock && !Data.IsStopping) // Releasing Pull
                 {
                     if (!Data.IsDown)
                         AdvancedHaptics.Rumble(__instance.HapticDeviceName == Raycaster.HapticDevice.Left, 0.001f, 40f, XConfig.PullTriggerPointerLockHaptic.Value / 100f);
 
                     Data.IsDown = false;
                     Data.IsStopping = true;
-                    Data.Coroutine = Plugin.Instance.StartCoroutine(UnblockDelay(__instance));
+                    Data.Coroutine = __instance.StartCoroutine(UnblockDelay(__instance));
                 }
             }
         }
@@ -117,7 +129,7 @@ namespace xsoverlay_tweak.Patches.Pointer
         [HarmonyPrefix]
         public static void BlockCursorMovement(Raycaster __instance, ref Vector2 ___DesktopCoordinates)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return;
+            if (!IsLockMode()) return;
 
             if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
                 if (Data.IsBlock)
@@ -130,7 +142,7 @@ namespace xsoverlay_tweak.Patches.Pointer
         [HarmonyPrefix]
         public static bool BlockPointerMovement(Raycaster __instance)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return true;
+            if (!IsLockMode()) return true;
             if (EventBridge.IsOverlayKeyboard(__instance.HoveringOverlay)) return true;
 
             if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
@@ -143,7 +155,7 @@ namespace xsoverlay_tweak.Patches.Pointer
         [HarmonyPrefix]
         public static bool BlockSearchForOverlays(Raycaster __instance)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return true;
+            if (!IsLockMode()) return true;
             if (EventBridge.IsOverlayKeyboard(__instance.HoveringOverlay)) return true;
 
             if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
@@ -156,7 +168,7 @@ namespace xsoverlay_tweak.Patches.Pointer
         [HarmonyPrefix]
         public static void InputClickLockPosition(Raycaster __instance, ref Vector2 ___DesktopCoordinates)
         {
-            if (!IsEnable() || !EventBridge.IsRaycasterHand(__instance)) return;
+            if (!IsLockMode()) return;
 
             if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
                 if (Data.IsBlock)
@@ -170,24 +182,44 @@ namespace xsoverlay_tweak.Patches.Pointer
             return !IsEnable();
         }
 
+        [HarmonyPatch(typeof(Raycaster), "HandleClicksForDesktopWindows")]
+        [HarmonyPatch(typeof(Raycaster), "HandleTouchInputForDesktopWindows")]
+        [HarmonyPatch(typeof(Raycaster), "HandleHeadWebAppInput")]
+        [HarmonyPatch(typeof(Raycaster), "BeginWebViewTouch")]
+        [HarmonyPatch(typeof(Raycaster), "BeginWebViewSinglePointer")]
+        [HarmonyPrefix]
+        public static void ListenClickInput(Raycaster __instance)
+        {
+            if (InstanceState.TryGetValue(__instance, out RaycasterState Data))
+                Data.WasClick = true;
+        }
+
+        [HarmonyPatch(typeof(XSettingsManager), nameof(XSettingsManager.SetSetting))]
+        [HarmonyPostfix]
+        public static void ListenSettingChanged(string name, string value, string value1, bool sendAnalytics = true)
+        {
+            if (name.Equals("PointerSmoothing"))
+                defaultSmoothing = Mathf.Clamp01(float.Parse(value, CultureInfo.InvariantCulture));
+        }
+
         private static IEnumerator UnblockDelay(Raycaster instance)
         {
-            yield return new WaitForSecondsRealtime(XSettingsManager.Instance.Settings.DoubleClickDelay);
-
             if (InstanceState.TryGetValue(instance, out RaycasterState Data))
             {
-                if (Data.WasSmooth)
-                    XSettingsManager.Instance.Settings.PointerSmoothing = pointerSmoothing;
+                yield return new WaitForSecondsRealtime(Data.WasClick ? XSettingsManager.Instance.Settings.DoubleClickDelay : 0f);
 
-                Data.WasSmooth = false;
+                if (Data.WasSmooth) // Restore smoothing when released
+                    XSettingsManager.Instance.Settings.PointerSmoothing = defaultSmoothing;
+
                 Data.IsBlock = false;
                 Data.IsStopping = false;
+                Data.WasClick = false;
+                Data.WasSmooth = false;
             }
         }
 
-        private static bool IsEnable()
-        {
-            return XConfig.PullTriggerPointerLock.Value != 0;
-        }
+        private static bool IsSmoothMode() => XConfig.PullTriggerPointerLock.Value is 3 or 4;
+        private static bool IsLockMode() => XConfig.PullTriggerPointerLock.Value is 1 or 2;
+        private static bool IsEnable() => XConfig.PullTriggerPointerLock.Value != 0;
     }
 }
