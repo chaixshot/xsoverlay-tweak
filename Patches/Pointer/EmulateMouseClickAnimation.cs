545bb980f22ed40cf103891563ff381d64b505e8
chaixshot <chai5142xshot@gmail.com>
2026-09-02 16:27:23 +0700

    Update EmulateMouseClickAnimation.cs

--- Patches/Pointer/EmulateMouseClickAnimation.cs
+++ Patches/Pointer/EmulateMouseClickAnimation.cs
@@ -1,17 +1,20 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using XSOverlay;
+using xsoverlay_tweak.Utils;
 
 namespace xsoverlay_tweak.Patches.Pointer
 {
     [HarmonyPatch(typeof(Raycaster))]
     internal class EmulateMouseClickAnimation
     {
-        [HarmonyPatch(nameof(Raycaster.HandleClicksForDesktopWindows))]
+        [HarmonyPatch("SendCapturedPressClick"), HarmonyPatch("SendCapturedPressDown")]
         [HarmonyPostfix]
-        public static void ShowAnimationWhenClicksForDesktopWindows(Raycaster __instance, ref GameObject ___VisualCursorElementClickAnimation, ref Unity_Overlay ___VisualCursorElementClickAnimationOverlay)
+        public static void ShowAnimationWhenClicksForDesktopWindows(Raycaster __instance, GameObject ___VisualCursorElementClickAnimation, Unity_Overlay ___VisualCursorElementClickAnimationOverlay, bool __result)
         {
             if (!IsEnable()) return;
+            if (!__result) return;
+            if (EventBridge.IsOverlayWebView(__instance.HoveringOverlay)) return;
 
             ___VisualCursorElementClickAnimation.transform.rotation = Quaternion.LookRotation(___VisualCursorElementClickAnimation.transform.position - Overlay_Manager.Instance.head.position);
             ___VisualCursorElementClickAnimationOverlay.gameObject.SetActive(value: true);
