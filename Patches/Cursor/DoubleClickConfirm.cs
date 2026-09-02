decdd28e3dd721c633fe479b66094a26213f0a79
chaixshot <chai5142xshot@gmail.com>
2026-09-02 16:09:29 +0700

    Update: DoubleClickConfirm

--- Patches/Cursor/DoubleClickConfirm.cs
+++ Patches/Cursor/DoubleClickConfirm.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using XSOverlay;
+using xsoverlay_tweak.Utils;
 
 namespace xsoverlay_tweak.Patches.Cursor
 {
@@ -25,126 +26,90 @@ namespace xsoverlay_tweak.Patches.Cursor
 
         [HarmonyPatch("Start")]
         [HarmonyPostfix]
-        public static void ListenForConfigChange()
+        public static void GetWindowsDoubleClickDelay()
         {
             wDoubleClickTime = GetDoubleClickTime() / 1000f;
         }
 
-        [HarmonyPatch("HandleClicksForDesktopWindows")]
-        [HarmonyPrefix]
-        public static bool WaitToConfrimDoubleClick(
-            Raycaster __instance,
-
-            ClickActions clickActions,
-            MouseInputDevice ___InputDevice,
-
-            bool ___HoldingTouch,
-            Vector2 ___DesktopCoordinates,
-            Vector2 ___CachedTouchPosition,
-
-            ref bool ___HadMouseInputDown,
-            ref bool ___HadMouseRightInputDown,
-            ref bool ___HadMouseMiddleInputDown
-            )
+        [HarmonyPatch("SendCapturedPressClick"), HarmonyPatch("SendCapturedPressDown")]
+        [HarmonyPostfix]
+        public static void WaitToConfrimDoubleClick(System.Reflection.MethodBase __originalMethod, Raycaster __instance, ref Vector2 ___CapturedPressDesktopCoordinate, int ___CapturedPressButton)
         {
-            if (!IsEnable()) return true;
+            if (!IsEnable()) return;
+            if (EventBridge.IsOverlayWebView(__instance.HoveringOverlay)) return;
 
             DoubleClickConfirmState DoubleClickState = InstanceState.GetOrCreateValue(__instance);
 
-            if (___InputDevice.InputSource == clickActions.InputSource)
-                if (!___HadMouseInputDown)
-                    if (__instance.CanClickDesktopCursor)
+            float delay = Time.time - DoubleClickState.lastClickTime;
+            bool isDoubleClickXSO = false;
+            bool isDoubleClickWin = false;
+            bool isWDoubleClick = delay <= wDoubleClickTime;
+            bool holdingTouch = __originalMethod.Name == "SendCapturedPressDown";
+
+            if (!isWDoubleClick && delay <= XSettingsManager.Instance.Settings.DoubleClickDelay)
+            {
+                isDoubleClickXSO = true;
+                DoubleClickState.lastClickTime = 0f;
+            }
+            else if (isWDoubleClick)
+            {
+                isDoubleClickWin = true;
+                DoubleClickState.lastClickTime = 0f;
+            }
+            else
+                DoubleClickState.lastClickTime = Time.time;
+
+            // Cache the cursor position and set it back when double-click to avoid the cursor moving from hand movement between clicks
+            if (isDoubleClickXSO || isDoubleClickWin)
+            {
+                ___CapturedPressDesktopCoordinate = lastDesktopCoordinates;
+                MouseOperations.SetCursorPosition((int)___CapturedPressDesktopCoordinate.x, (int)___CapturedPressDesktopCoordinate.y);
+            }
+            else if (!holdingTouch)
+                lastDesktopCoordinates = ___CapturedPressDesktopCoordinate;
+
+            if (!isDoubleClickWin && isDoubleClickXSO)
+            {
+                if (!holdingTouch) // Handle standard clicks - double clicks (SendCapturedPressClick)
+                {
+                    switch (___CapturedPressButton)
                     {
-                        float delay = Time.time - DoubleClickState.lastClickTime;
-                        bool isDoubleClickXSO = false;
-                        bool isDoubleClickWin = false;
-                        bool isWDoubleClick = delay <= wDoubleClickTime;
-
-                        if (!isWDoubleClick && delay <= XSettingsManager.Instance.Settings.DoubleClickDelay)
-                        {
-                            isDoubleClickXSO = true;
-                            DoubleClickState.lastClickTime = 0f;
-                        }
-                        else if (isWDoubleClick)
-                        {
-                            isDoubleClickWin = true;
-                            DoubleClickState.lastClickTime = 0f;
-                        }
-                        else
-                            DoubleClickState.lastClickTime = Time.time;
-
-                        // Cache the cursor position and set it back when double-click to avoid the cursor moving from hand movement between clicks
-                        if (isDoubleClickXSO || isDoubleClickWin)
-                            MouseOperations.SetCursorPosition((int)lastDesktopCoordinates.x, (int)lastDesktopCoordinates.y);
-                        else
-                            if (!___HoldingTouch)
-                                lastDesktopCoordinates = ___DesktopCoordinates;
-                            else
-                                lastDesktopCoordinates = ___CachedTouchPosition;
-
-                        if (isDoubleClickXSO || isDoubleClickWin)
-                        {
-                            if (!clickActions.IsHoldingMouseClick)
-                            {
-                                // Handle standard clicks / double clicks
-                                switch (clickActions.ActionIndex)
-                                {
-                                    case 0: // Left
-                                        if (isDoubleClickXSO)
-                                            InputManager.sim.Mouse.LeftButtonDoubleClick();
-                                        else
-                                            InputManager.sim.Mouse.LeftButtonClick();
-                                        break;
-                                    case 1: // Right
-                                        if (isDoubleClickXSO)
-                                            InputManager.sim.Mouse.RightButtonDoubleClick();
-                                        else
-                                            InputManager.sim.Mouse.RightButtonClick();
-                                        break;
-                                    case 2: // Middle
-                                        if (isDoubleClickXSO)
-                                            InputManager.sim.Mouse.MiddleButtonClick();
-                                        InputManager.sim.Mouse.MiddleButtonClick();
-                                        break;
-                                }
-
-                                return false;
-                            }
-                            else if (!clickActions.ShouldRelease)
-                            {
-                                // Handle Click & Hold
-                                AnimateCursorHold(__instance, true);
-
-                                switch (clickActions.ActionIndex)
-                                {
-                                    case 0: // Left
-                                        ___HadMouseInputDown = true;
-                                        if (isDoubleClickXSO)
-                                            MouseOperations.LMouseClick(InputManager.sim); // Fixes original XSO double-click quirk
-                                        MouseOperations.LMouseDown(InputManager.sim);
-                                        break;
-
-                                    case 1: // Right
-                                        ___HadMouseRightInputDown = true;
-                                        if (isDoubleClickXSO)
-                                            MouseOperations.RMouseClick(InputManager.sim);
-                                        MouseOperations.RMouseDown(InputManager.sim);
-                                        break;
-
-                                    case 2: // Middle
-                                        ___HadMouseMiddleInputDown = true;
-                                        if (isDoubleClickXSO)
-                                            MouseOperations.MMouseClick(InputManager.sim);
-                                        MouseOperations.MMouseDown(InputManager.sim);
-                                        break;
-                                }
-
-                                return false;
-                            }
-                        }
+                        case 0: // Left
+                            InputManager.sim.Mouse.LeftButtonDoubleClick();
+                            break;
+                        case 1: // Right
+                            InputManager.sim.Mouse.RightButtonDoubleClick();
+                            break;
+                        case 2: // Middle
+                            InputManager.sim.Mouse.MiddleButtonClick();
+                            InputManager.sim.Mouse.MiddleButtonClick();
+                            break;
                     }
+                }
+                else // Handle Click - Relase - Click Hold (SendCapturedPressDown)
+                {
+                    AnimateCursorHold(__instance, true);
 
-            return true;
+                    switch (___CapturedPressButton)
+                    {
+                        case 0: // Left
+                            MouseOperations.LMouseUp(InputManager.sim);
+                            MouseOperations.LMouseDown(InputManager.sim);
+                            break;
+
+                        case 1: // Right
+                            MouseOperations.RMouseUp(InputManager.sim);
+                            MouseOperations.RMouseDown(InputManager.sim);
+                            break;
+
+                        case 2: // Middle
+                            MouseOperations.MMouseUp(InputManager.sim);
+                            MouseOperations.MMouseDown(InputManager.sim);
+                            break;
+
+                    }
+                }
+            }
         }
 
         private static bool IsEnable()
